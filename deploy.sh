f6fe6e984162ee4a1e8563a01ae39eab685f22b6
octopus <912554887@qq.com>
2025-07-31 19:50:38 +0800

    modify config files

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ set -ex
 
 chmod +x *.sh
 
-cp ./conf/application-prod.yml ./conf/application.yml
-cp ./conf/log4j2-example.xml ./conf/log4j2.xml
+#cp ./conf/application-prod.yml ./conf/application.yml
+#cp ./conf/log4j2-example.xml ./conf/log4j2.xml
 
 # #停止服务
 if [ x"[@SC_ROUTER_STOP_SCRIPT]" != x"" ];then

7f488307dcde1bc5b852814831a86c7c8346e972
octopus <912554887@qq.com>
2025-07-31 19:40:54 +0800

    <fix>: copy config files before start service

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@ set -ex
 
 chmod +x *.sh
 
+cp ./conf/application-prod.yml ./conf/application.yml
+cp ./conf/log4j2-example.xml ./conf/log4j2.xml
+
 # #停止服务
 if [ x"[@SC_ROUTER_STOP_SCRIPT]" != x"" ];then
   ! sh [@SC_ROUTER_STOP_SCRIPT]
 fi
-
-cp ./conf/application-prod.yml ./conf/application.yml
-cp ./conf/log4j2-example.xml ./conf/log4j2.xml
 
 sh [@SC_ROUTER_START_SCRIPT]
 
